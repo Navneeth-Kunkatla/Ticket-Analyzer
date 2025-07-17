d9113a7d41506319b73e5d61383790c431f938bd
Navneeth-Kunkatla <86120683+Navneeth-Kunkatla@users.noreply.github.com>
2025-07-17 16:36:06 +0530

    Delete path_finder.py

--- path_finder.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def get_file_paths_dict(root_directory):
-    file_paths_dict = {}
-    for foldername, subfolders, filenames in os.walk(root_directory):
-        for filename in filenames:
-            full_path = os.path.join(foldername, filename)
-            file_paths_dict[filename] = full_path
-    return file_paths_dict
